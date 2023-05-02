797fab2aee4c26f9226acfff7b2d80b34c68e1d6
hyeonpearl <hc9064@gmail.com>
2023-05-02 18:16:44 +0900

    FEAT: Global 스타일 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,36 @@
+import Header from '@/components/Header';
+import { Global, css } from '@emotion/react';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Global
+        styles={css`
+          ::selection {
+            background: #2cab74;
+          }
+
+          html,
+          body {
+            background: #191919;
+            color: #fff;
+            margin: 0;
+            word-break: keep-all;
+            font-family: Pretendard, -apple-system, BlinkMacSystemFont,
+              Bazier Square, Noto Sans KR, Segoe UI, Apple SD Gothic Neo, Roboto,
+              Helvetica Neue, Arial, sans-serif, Apple Color Emoji,
+              Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;
+            letter-spacing: -0.3px;
+            padding-bottom: 2rem;
+            overflow-x: hidden;
+            -webkit-font-smoothing: antialiased;
+            -moz-osx-font-smoothing: grayscale;
+          }
+        `}
+      />
+      <Header />
+      <Component {...pageProps} />
+    </>
+  );
 }
