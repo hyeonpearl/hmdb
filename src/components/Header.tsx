d250d9a1335c89f3a8514b0a4edd1f7b03519fbd
hyeonpearl <hc9064@gmail.com>
2023-05-03 13:41:16 +0900

    FIX: Header 컴포넌트 스타일 수정
    * _app 파일에 common 변수 적용

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,11 @@
-import styled from '@emotion/styled';
-import { AppBar, Container, Grid, Toolbar } from '@mui/material';
+import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react';
+
+import styled from '@emotion/styled';
+import { AppBar, Container, Grid, Toolbar } from '@mui/material';
+
+import { common } from '@/styles/common';
 
 export default function Header() {
   return (
@@ -12,7 +15,7 @@ export default function Header() {
           <Link href={'/'} className='logo'>
             <Image
               src={'/logo_blank.svg'}
-              width={140}
+              width={160}
               height={62.5}
               alt='logo'
             />
@@ -21,13 +24,10 @@ export default function Header() {
           <nav>
             <Grid container alignItems={'center'} spacing={8}>
               <Grid item xs={4}>
-                <Item href={'/test'}>Trending</Item>
+                <Item href={'/test'}>영화</Item>
               </Grid>
               <Grid item xs={4}>
-                <Item href={'/test'}>Popular</Item>
-              </Grid>
-              <Grid item xs={4}>
-                <Item href={'/test'}>Top&nbsp;Rate</Item>
+                <Item href={'/test'}>TV&nbsp;프로그램</Item>
               </Grid>
             </Grid>
           </nav>
@@ -38,20 +38,25 @@ export default function Header() {
 }
 
 const StyledAppBar = styled(AppBar)`
-  background: #323232;
+  background: ${common.color.gray01};
   position: relative;
 `;
 
 const StyledToolbar = styled(Toolbar)`
   justify-content: space-between;
+
+  @media screen and (max-width: 390px) {
+    a img {
+      width: 100px;
+    }
+  }
 `;
 
 const Item = styled(Link)`
   text-decoration: none;
-  color: #fff;
-  width: 6rem;
+  color: ${common.color.white};
 
   :hover {
-    color: #ccc;
+    color: ${common.color.gray_hover};
   }
 `;
