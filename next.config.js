069d7cbaff569266eeeb705d421d8e4ddd6d3a6e
hyeonpearl <hc9064@gmail.com>
2023-05-10 15:08:53 +0900

    Feat: TV Show API 추가 등록
    * Trending, Top Rated

--- next.config.js
+++ next.config.js
@@ -15,6 +15,14 @@ const nextConfig = {
         source: '/api/movies/top_rated',
         destination: `https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.API_KEY}&language=ko&page=1&region=KR`,
       },
+      {
+        source: '/api/tv/trending',
+        destination: `https://api.themoviedb.org/3/trending/tv/day?api_key=${process.env.API_KEY}&language=ko&region=KR`,
+      },
+      {
+        source: '/api/tv/top_rated',
+        destination: `https://api.themoviedb.org/3/tv/top_rated?api_key=${process.env.API_KEY}&language=ko&page=1&region=KR`,
+      },
     ];
   },
 };
