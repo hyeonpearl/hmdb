bf3b9e4c9d97d601f72febfb7b136197559db0db
hyeonpearl <hc9064@gmail.com>
2023-05-12 17:34:57 +0900

    Fix: Detail 페이지 스타일 수정
    * Home 페이지 Intro 스타일 수정
    
    * rewrites url 수정

--- src/pages/movie/[id].tsx
+++ src/pages/movie/[id].tsx
@@ -3,10 +3,17 @@ import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 
 import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+import BarChartRoundedIcon from '@mui/icons-material/BarChartRounded';
 import StarRateRoundedIcon from '@mui/icons-material/StarRateRounded';
 
 import { common } from '@/styles/common';
 import SEO from '@/components/SEO';
+
+interface Genre {
+  id: number;
+  name: string;
+}
 
 interface MovieInfo {
   title: string;
@@ -16,6 +23,10 @@ interface MovieInfo {
   vote_count: number;
   poster_path: string;
   backdrop_path: string;
+  popularity: number;
+  tagline: string;
+  overview: string;
+  genres: Genre[];
 }
 
 export default function Detail() {
@@ -29,6 +40,10 @@ export default function Detail() {
     vote_count: 0,
     poster_path: '',
     backdrop_path: '',
+    popularity: 0,
+    tagline: '',
+    overview: '',
+    genres: [{ id: 0, name: '' }],
   });
 
   useEffect(() => {
@@ -43,48 +58,83 @@ export default function Detail() {
     maximumFractionDigits: 1,
   }).format(movieInfo.vote_count);
 
+  const popular_count = new Intl.NumberFormat('en-US', {
+    notation: 'compact',
+    maximumFractionDigits: 0,
+  }).format(movieInfo.popularity);
+
+  const backdrop = `https://image.tmdb.org/t/p/w500/${movieInfo.backdrop_path}`;
+
+  const genres = movieInfo.genres.map((el: Genre) => (
+    <li key={el.id}>{el.name}</li>
+  ));
+
+  console.log(movieInfo);
+
   return (
     <>
       <SEO title={movieInfo.title} />
 
-      <StyledMain>
+      <StyledMain backdrop={backdrop}>
         <section>
-          <article>
-            <div className='movie_title'>
-              <h1>{movieInfo.title}</h1>
-              <div>
-                <span>
-                  {movieInfo.original_title} • {movieInfo.runtime}m
-                </span>
-              </div>
-            </div>
-
-            <div className='movie_rating'>
-              <div className='rating_info'>
-                <p>HMDb 평점</p>
-                <div className='rating_detail'>
-                  <StarRateRoundedIcon />
-                  <div>
-                    <div className='rating_score'>
-                      <strong>{movieInfo.vote_average?.toFixed(1)}</strong>
-                      <span>&nbsp;/ 10</span>
-                    </div>
-                    <div className='rating_count'>{vote_count}</div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </article>
-
-          <article>
+          <article className='poster'>
             <img
               src={`https://image.tmdb.org/t/p/w500/${movieInfo.poster_path}`}
               alt='poster'
             />
-            <img
-              src={`https://image.tmdb.org/t/p/w500/${movieInfo.backdrop_path}`}
-              alt='backdrop'
-            />
+          </article>
+
+          <article className='movie_detail'>
+            <hgroup className='movie_title'>
+              <h1>{movieInfo.title}</h1>
+
+              <h3>
+                {movieInfo.original_title} • {movieInfo.runtime}m
+              </h3>
+            </hgroup>
+
+            <ul className='movie_rating'>
+              <li>
+                <h4>HMDb 평점</h4>
+
+                <div className='vote_detail'>
+                  <StarRateRoundedIcon />
+
+                  <div>
+                    <div className='vote_score'>
+                      <strong>{movieInfo.vote_average?.toFixed(1)}</strong>
+                      <span>&nbsp;/ 10</span>
+                    </div>
+
+                    <div className='vote_count'>{vote_count}</div>
+                  </div>
+                </div>
+              </li>
+
+              <li>
+                <h4>인기도</h4>
+
+                <div className='popular_detail'>
+                  <BarChartRoundedIcon />
+                  <strong>{popular_count}</strong>
+                </div>
+              </li>
+            </ul>
+
+            <p className='tagline'>{movieInfo.tagline}</p>
+
+            <div className='overview'>
+              {movieInfo.overview !== '' ? (
+                <>
+                  <h4>개요</h4>
+                  <p>{movieInfo.overview}</p>
+                </>
+              ) : (
+                ''
+              )}
+            </div>
+
+            <ul className='genres'>{genres}</ul>
           </article>
         </section>
       </StyledMain>
@@ -96,88 +146,198 @@ const StyledMain = styled.main`
   margin: 66px 0;
 
   section {
-    article {
-      margin: 0 5rem;
+    :first-of-type {
+      display: flex;
+      padding: 2rem;
 
-      :first-of-type {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
+      ${({ backdrop }: { backdrop: string }) =>
+        backdrop &&
+        css`
+          position: relative;
+          overflow: hidden;
+
+          ::before {
+            content: '';
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-image: url(${backdrop});
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-position: center;
+            filter: blur(10px);
+            opacity: 0.3;
+            z-index: 0;
+          }
+        `}
+
+      article {
+        position: relative;
+        z-index: 1;
+      }
+
+      article.poster {
+        img {
+          height: 35rem;
+        }
+      }
+
+      article.movie_detail {
+        margin: 0 5rem;
         padding: 3rem 0;
+        display: flex;
+        flex-direction: column;
 
-        .movie_title {
+        hgroup.movie_title {
+          margin-bottom: 2rem;
+
           h1 {
             margin: 0;
             font-size: 3rem;
           }
 
-          span {
+          h3 {
             color: ${common.color.gray06};
             font-weight: 500;
           }
         }
 
-        .movie_rating {
-          .rating_info {
-            display: flex;
-            flex-direction: column;
+        ul.movie_rating {
+          display: flex;
+          margin-bottom: 2rem;
 
-            p {
-              margin: 0;
-              margin-bottom: 2px;
-              font-weight: 500;
-              color: ${common.color.gray06};
-              text-align: center;
-            }
-
-            .rating_detail {
+          li {
+            :first-of-type {
               display: flex;
-              cursor: pointer;
-              padding: 0 0.5rem;
-              border-radius: 5px;
+              flex-direction: column;
 
-              :hover {
-                background: ${common.color.gray02};
-                transition: all 0.3s ease;
+              h4 {
+                margin-bottom: 2px;
+                font-weight: 500;
+                color: ${common.color.gray06};
+                text-align: center;
               }
 
-              svg {
-                color: ${common.color.green};
-                font-size: 2.5rem;
-              }
-
-              div {
+              .vote_detail {
                 display: flex;
-                flex-direction: column;
+                padding: 0 0.5rem;
+                cursor: pointer;
 
-                .rating_score {
-                  flex-direction: row;
-                  align-items: center;
+                :hover {
+                  border-radius: 5px;
+                  background: ${common.color.gray02};
+                  transition: all 0.3s ease;
+                }
 
-                  strong {
-                    font-size: 1.25rem;
+                svg {
+                  color: ${common.color.green};
+                  font-size: 2.5rem;
+                }
+
+                div {
+                  display: flex;
+                  flex-direction: column;
+
+                  .vote_score {
+                    flex-direction: row;
+                    align-items: center;
+
+                    strong {
+                      font-size: 1.25rem;
+                    }
+
+                    span {
+                      font-size: 0.85rem;
+                      color: ${common.color.gray06};
+                    }
                   }
 
-                  span {
+                  .vote_count {
                     font-size: 0.85rem;
                     color: ${common.color.gray06};
                   }
                 }
+              }
+            }
 
-                .rating_count {
-                  font-size: 0.85rem;
-                  color: ${common.color.gray06};
+            :last-of-type {
+              margin-left: 1rem;
+              display: flex;
+              flex-direction: column;
+
+              h4 {
+                margin-bottom: 2px;
+                font-weight: 500;
+                color: ${common.color.gray06};
+                text-align: center;
+              }
+              div.popular_detail {
+                display: flex;
+                align-items: center;
+                padding: 0 0.5rem;
+                cursor: pointer;
+
+                :hover {
+                  border-radius: 5px;
+                  background: ${common.color.gray02};
+                  transition: all 0.3s ease;
+                }
+
+                svg {
+                  color: ${common.color.yellow};
+                  font-size: 2.5rem;
+                }
+
+                strong {
+                  font-size: 1.25rem;
                 }
               }
             }
           }
         }
-      }
 
-      :nth-of-type(2) {
-        img {
-          height: 20rem;
-          margin-right: 1rem;
+        p.tagline {
+          margin-bottom: 1.5rem;
+          font-style: italic;
+          font-size: 1.05rem;
+          color: ${common.color.gray06};
+        }
+
+        div.overview {
+          margin-bottom: 1.5rem;
+
+          h4 {
+            margin-bottom: 0.5rem;
+            font-size: 1.25rem;
+          }
+        }
+
+        ul.genres {
+          display: flex;
+
+          li {
+            color: ${common.color.white};
+            padding: 0 0.5rem;
+            border-radius: 5px;
+            background: ${common.color.gray02};
+            cursor: pointer;
+
+            :hover {
+              background: linear-gradient(
+                to right,
+                ${common.color.green},
+                ${common.color.yellow}
+              );
+              color: ${common.color.black};
+              transition: all 0.3s ease;
+            }
+
+            :not(:first-of-type) {
+              margin-left: 1rem;
+            }
+          }
         }
       }
     }
