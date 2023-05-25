d800a5365acdf35dc5edaff1505d80ef6a7f6673
hyeonpearl <hc9064@gmail.com>
2023-05-25 15:16:48 +0900

    Feat: Movie Category 페이지 추가

--- src/pages/movie/now_playing.tsx
+++ src/pages/movie/now_playing.tsx
@@ -1,3 +1,49 @@
+import styled from '@emotion/styled';
+
+import { common } from '@/styles/common';
+import SEO from '@/components/SEO';
+import MovieContainer from '@/containers/MovieContainer';
+
 export default function NowPlaying() {
-  return <>현재 상영 중</>;
+  return (
+    <>
+      <SEO title={'현재 상영 중'} />
+      <StyledMain>
+        <h3>
+          <span>현재 상영 중</span>
+        </h3>
+
+        <section>
+          <MovieContainer request='now_playing' color='white' layout='column' />
+        </section>
+      </StyledMain>
+    </>
+  );
 }
+
+const StyledMain = styled.main`
+  margin-top: 66px;
+  padding: 0 3rem;
+
+  h3 {
+    padding: 5rem 0;
+    text-align: center;
+    font-size: 4rem;
+
+    span {
+      cursor: pointer;
+
+      :hover {
+        background: linear-gradient(
+          to right,
+          ${common.color.green},
+          ${common.color.yellow}
+        );
+        background-clip: text;
+        -webkit-background-clip: text;
+        color: transparent;
+        transition: color 0.3s ease;
+      }
+    }
+  }
+`;
--- src/pages/movie/top_rate.tsx
+++ src/pages/movie/top_rate.tsx
@@ -1,3 +1,49 @@
+import styled from '@emotion/styled';
+
+import { common } from '@/styles/common';
+import SEO from '@/components/SEO';
+import MovieContainer from '@/containers/MovieContainer';
+
 export default function TopRate() {
-  <>평점 높은 순</>;
+  return (
+    <>
+      <SEO title={'평점 높은 영화'} />
+      <StyledMain>
+        <h3>
+          <span>평점 높은 영화</span>
+        </h3>
+
+        <section>
+          <MovieContainer request='top_rated' color='white' layout='column' />
+        </section>
+      </StyledMain>
+    </>
+  );
 }
+
+const StyledMain = styled.main`
+  margin-top: 66px;
+  padding: 0 3rem;
+
+  h3 {
+    padding: 5rem 0;
+    text-align: center;
+    font-size: 4rem;
+
+    span {
+      cursor: pointer;
+
+      :hover {
+        background: linear-gradient(
+          to right,
+          ${common.color.green},
+          ${common.color.yellow}
+        );
+        background-clip: text;
+        -webkit-background-clip: text;
+        color: transparent;
+        transition: color 0.3s ease;
+      }
+    }
+  }
+`;
