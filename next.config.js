bdd96eb183e3596c49db99706b0797cd1037ec5e
hyeonpearl <hc9064@gmail.com>
2023-05-27 16:13:11 +0900

    Feat: TV on_air API 추가

--- next.config.js
+++ next.config.js
@@ -5,16 +5,16 @@ const nextConfig = {
     return [
       // 영화 리스트 API
       {
-        source: '/api/movie/now_playing',
-        destination: `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.API_KEY}&language=ko&page=1&region=KR`,
-      },
-      {
         source: '/api/movie/popular',
         destination: `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&language=ko&page=1&region=KR`,
       },
       {
         source: '/api/movie/top_rated',
         destination: `https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.API_KEY}&language=ko&page=1&region=KR`,
+      },
+      {
+        source: '/api/movie/now_playing',
+        destination: `https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.API_KEY}&language=ko&page=1&region=KR`,
       },
       // 영화 상세 정보 API
       {
@@ -34,6 +34,10 @@ const nextConfig = {
       {
         source: '/api/tv/top_rated',
         destination: `https://api.themoviedb.org/3/tv/top_rated?api_key=${process.env.API_KEY}&language=ko&page=1`,
+      },
+      {
+        source: '/api/tv/on_air',
+        destination: `https://api.themoviedb.org/3/tv/on_the_air?api_key=${process.env.API_KEY}&language=ko&page=1`,
       },
       // TV 상세 정보 API
       {
---
+++ src/pages/tvShow/on_air.tsx
@@ -0,0 +1,49 @@
+import styled from '@emotion/styled';
+
+import { common } from '@/styles/common';
+import SEO from '@/components/SEO';
+import TVContainer from '@/containers/TVContainer';
+
+export default function OnAir() {
+  return (
+    <>
+      <SEO title={'TV 방영 중'} />
+      <StyledMain>
+        <h3>
+          <span>TV 방영 중</span>
+        </h3>
+
+        <section>
+          <TVContainer request='on_air' color='white' layout='wrap' />
+        </section>
+      </StyledMain>
+    </>
+  );
+}
+
+const StyledMain = styled.main`
+  margin-top: 66px;
+  padding: 0 3rem;
+
+  h3 {
+    padding: 5rem 0;
+    text-align: center;
+    font-size: 4rem;
+
+    span {
+      cursor: pointer;
+
+      :hover {
+        background: linear-gradient(
+          to right,
+          ${common.color.green},
+          ${common.color.yellow}
+        );
+        background-clip: text;
+        -webkit-background-clip: text;
+        color: transparent;
+        transition: color 0.3s ease;
+      }
+    }
+  }
+`;
